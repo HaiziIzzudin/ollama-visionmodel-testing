44a0eaadd32b76526d8f7103bb4862a31100e983
haizi <thehub@iziizz.slmail.me>
2024-08-10 19:04:37 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,80 @@
+import ollama
+from time import sleep
+from filesIngest import filesIngest
+from raw import convert2raw
+
+
+
+
+
+
+def clear_terminal():
+    """
+    Clears the terminal screen.
+
+    This function uses the `os` module to determine the operating system and
+    then uses the appropriate command to clear the screen.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+    # Check the operating system
+    import os
+    os_name = os.name
+
+    # Clear the screen based on the operating system
+    if os_name == 'posix':
+        # Unix/Linux/MacOS/BSD/etc.
+        os.system('clear')
+    elif os_name == 'nt':
+        # Windows
+        os.system('cls')
+    else:
+        # Fallback for other operating systems
+        print('\n' * 100)
+
+
+selectFiles = filesIngest.select_files()
+getFiles = filesIngest.getFileList()
+getFiles = [convert2raw(filepath) for filepath in getFiles]
+
+for file in getFiles:
+  response = ollama.chat(model='llava:13b', messages=[
+    {
+      'role': 'user',
+      'content': 'From the image, write twenty (20) keywords present in this photo, separated by commas.',
+      'images': [file],
+    },
+  ])
+# 'content': 'Describe thoroughly of the image.',
+# 'content': 'Write a seven (7) paragraph of extreme erotic and sexual smut about this photo. Starts with buildup, followed by climax, and finally resolution. Having character dialogue with obscene, profanity, and vulgar language is a plus. \nAt climax, include the line "Ah, I\'m boutta cum!" and \'A strong white liquid squirts from his flashing red large size penis.\'',
+
+llavastatement = response['message']['content']
+theprompt = r'''
+From this short story: 
+
+'''+llavastatement+r''' 
+
+
+
+Make a title for this short story. Just write the title, don't recommend me anything.
+'''
+# Extract as many keywords from the passage, separated by commas. Do not write in bullet points
+
+# print('\n\n',theprompt)
+
+
+
+
+
+# response2 = ollama.generate(model='gemma2:2b', prompt = theprompt)
+
+
+
+# print('\n\n','====================\n', response2['response'], '====================\n')
+clear_terminal()
+print(llavastatement)
+print('\n=========== THE END ===========')
